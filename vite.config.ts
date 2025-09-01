6bf5d9958df690092018b03a21955db4d93cb3fc
Alfej Mansuri <alfejm@maqsoftware.com>
2025-09-01 16:59:44 +0530

    Working Electron app

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Important for Electron
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"), // Add this line
     },
   },
-  base: './', // Important for Electron
   build: {
-    outDir: 'build',
-    sourcemap: false,
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   server: {
-    port: 5173,
-  },
-})
+    port: 5173
+  }
+});
